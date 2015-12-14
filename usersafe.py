7c13609e4f47911636b1a31f7775ec09c105b39a
Alex Doler <aedoler@gmail.com>
2015-12-14 03:21:48 +0000

    .

--- usersafe.py
+++ usersafe.py
@@ -5,50 +5,83 @@
 import pickle
 import os
 
-USERINPUT = raw_input('Would you like to enter a new password or '
-                      'look up an old one? Enter: "new" or "look up" ')
-USERINPUT = USERINPUT.lower()
+RUN_PROGRAM = True
 
-USERDICT = {}
 
-if os.path.exists('user_info.pkl'):
-    FILEHANDLER = open('user_info.pkl', 'rb')
-    USERDICT = pickle.load(FILEHANDLER)
-    FILEHANDLER.close()
+def usersafe():
+    """ Program to add new and look up user informatino from a file.
+    Args:
+        None - defaults to True so program will continue running.
+    Returns:
+        Allows user to input user information for a website or retrieve
+        previousy input information.
+    Examples:
+    >>>Would you like to enter a new password or look up an old one?
+    Enter: "new" or "look up" new
+    Please enter the name of the website. Ex.: facebook.com: WEBSITE
+    Please enter your username for this website: USERNAME
+    Please enter your password for this website: PASSWORD
 
-if USERINPUT == 'new':
-    ENTERNEW = raw_input('Please enter the name of the website. '
-                         'Ex.: facebook.com ')
-    ENTERNEW = ENTERNEW.lower()
-    ENTERNEW_NAME = raw_input('Please enter your username for this website ')
-    ENTERNEW_NAME = ENTERNEW_NAME.lower()
-    ENTERNEW_PASS = raw_input('Please enter you password for this website ')
-    ENTERNEW_PASS = ENTERNEW_PASS.lower()
+    >>>Would you like to enter a new password or look up an old one?
+    Enter: "new" or "look up" look up
+    Please enter the website for which you would like to look up
+    your user information: facebook.com
+                USERNAME: username
+                PASSWORD: password
+    """
+    userinput = raw_input('Would you like to enter a new password or \
+look up an old one? Enter: "new" or "look up" ')
+    userinput = userinput.lower()
 
-    if ENTERNEW not in USERDICT:
+    userdict = {}
 
-        USERDICT[ENTERNEW] = (ENTERNEW_NAME, ENTERNEW_PASS)
-        FILEHANDLER = open('user_info.pkl', 'wb')
-        pickle.dump(USERDICT, FILEHANDLER)
-        FILEHANDLER.close()
+    if os.path.exists('user_info.pkl'):
+        filehandler = open('user_info.pkl', 'rb')
+        userdict = pickle.load(filehandler)
+        filehandler.close()
 
-    elif ENTERNEW in USERDICT:
-        UPDATEDICT = {}
-        UPDATEDICT[ENTERNEW] = (ENTERNEW_NAME, ENTERNEW_PASS)
-        USERDICT.update(UPDATEDICT)
-        FILEHANDLER = open('user_info.pkl', 'wb')
-        pickle.dump(USERDICT, FILEHANDLER)
-        FILEHANDLER.close()
+    if userinput == 'new':
+        enternew = raw_input('Please enter the name of the website. '
+                             'Ex.: facebook.com: ')
+        enternew = enternew.lower()
+        enternew_name = raw_input('Please enter your username for this \
+website: ')
+        enternew_name = enternew_name.lower()
+        enternew_pass = raw_input('Please enter your password for \
+this website: ')
+        enternew_pass = enternew_pass.lower()
 
-elif USERINPUT == 'look up':
-    LOOKUP = False
-    while LOOKUP not in USERDICT:
-        LOOKUP = raw_input('Please enter the website for which you would \
-like to look up your user information ')
+        if enternew not in userdict:  # adds new input
 
-    USERINFO = 'Your user information for the website "{}" is as follows:\n\
-                USERNAME: {}\n\
-                PASSWORD: {}'
+            userdict[enternew] = (enternew_name, enternew_pass)
+            filehandler = open('user_info.pkl', 'wb')
+            pickle.dump(userdict, filehandler)
+            filehandler.close()
+            print '\nYou have successfully entered your user information!\n'
+            return None
 
-    print USERINFO.format(LOOKUP.upper(), USERDICT[LOOKUP][0],
-                          USERDICT[LOOKUP][1])
+        elif enternew in userdict:  # replaces old input if same website
+            updatedict = {}
+            updatedict[enternew] = (enternew_name, enternew_pass)
+            userdict.update(updatedict)
+            filehandler = open('user_info.pkl', 'wb')
+            pickle.dump(userdict, filehandler)
+            filehandler.close()
+
+    elif userinput == 'look up':
+        lookup = False
+        while lookup not in userdict:
+            lookup = raw_input('Please enter the website for which you would \
+like to look up your user information: ')
+
+        userinfo = 'Your user information for the website "{}"\
+is as follows:\n\n\
+                    USERNAME: {}\n\
+                    PASSWORD: {}\n\n\n'
+
+    print userinfo.format(lookup.upper(), userdict[lookup][0],
+                          userdict[lookup][1])
+    return None
+
+while RUN_PROGRAM:  # keeps program running
+    usersafe()
